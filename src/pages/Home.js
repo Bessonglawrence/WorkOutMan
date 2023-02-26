80f9193026be1bfbee69c462f9f9bb3172e45357
Base <orockbessonglawrence@gmail.com>
2023-02-26 16:23:50 +0000

    Created a home file and made an API call

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react'
+
+
+const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(()=>{
+        const fetchWorkouts = async () =>{
+            const response = await fetch('/api/workouts');
+            const json = await response.json();
+
+            if(response.ok){
+                setWorkouts(json);
+            }
+        }
+        fetchWorkouts();
+    }, [])
+  return (
+   <div className='home'>
+       <div className='workouts'>
+            {workouts && workouts.map((workout) =>{
+               return <p key={workout._id}>{workout.title}</p>
+            })}
+       </div>
+   </div>
+  )
+}
+
+export default Home;
